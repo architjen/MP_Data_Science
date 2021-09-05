6efb2c04142d5d3c840b88f970bd8d02362fa256
architjen <architjen010@gmail.com>
2021-09-06 00:18:08 +0200

    last app.py changes

--- app.py
+++ app.py
@@ -22,12 +22,26 @@ def predict():
     '''
     For rendering results on HTML GUI
     '''
+    #get the list of features
     float_features = [float(x) for x in request.form.values()]
+    
+    #converts them to 2d array
     final_features = [np.array(float_features)]
+    
+    #apply the normalisation on the features
     scaled_final_features = scaler.transform(final_features)
+    
+    #predict the features
     prediction = model.predict(scaled_final_features)
 
-    return render_template('index.html', prediction_text='The player would be $ {}'.format(prediction))
+    #returning the statement instead of 0 and 1
+    if prediction == 1:
+        ans = 'would last 5 years in NBA'
+    else:
+        ans = 'wouldnt last 5 years in NBA'
+
+
+    return render_template('index.html', prediction_text='The player {}'.format(ans))
 
 if __name__ == "__main__":
     app.run(debug=True)
