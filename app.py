c8e1d1c8e1d1474a03c4bb4b3a02579e15809a3e
architjen <architjen010@gmail.com>
2021-09-05 23:25:01 +0200

    normalising features

--- app.py
+++ app.py
@@ -10,6 +10,9 @@ app = Flask(__name__)
 #load model
 model = pickle.load(open('model.pkl', 'rb'))
 
+#scaler for normalising the features
+scaler = pickle.load(open('scaler.pkl', 'rb'))
+
 @app.route('/')
 def home():
     return render_template('index.html')
@@ -21,9 +24,8 @@ def predict():
     '''
     float_features = [float(x) for x in request.form.values()]
     final_features = [np.array(float_features)]
-    prediction = model.predict(final_features)
-
-    
+    scaled_final_features = scaler.transform(final_features)
+    prediction = model.predict(scaled_final_features)
 
     return render_template('index.html', prediction_text='The player would be $ {}'.format(prediction))
 
