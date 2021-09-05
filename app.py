54af86dff331b07021f34b48c59828adab2bf942
architjen <architjen010@gmail.com>
2021-09-05 22:03:21 +0200

    some changes

--- app.py
+++ app.py
@@ -4,7 +4,10 @@ import numpy as np
 from flask import Flask, request, render_template
 import pickle
 
+# create Flask app
 app = Flask(__name__)
+
+#load model
 model = pickle.load(open('model.pkl', 'rb'))
 
 @app.route('/')
@@ -16,13 +19,13 @@ def predict():
     '''
     For rendering results on HTML GUI
     '''
-    int_features = [int(x) for x in request.form.values()]
-    final_features = [np.array(int_features)]
+    float_features = [float(x) for x in request.form.values()]
+    final_features = [np.array(float_features)]
     prediction = model.predict(final_features)
 
-    output = round(prediction[0], 2)
+    
 
-    return render_template('index.html', prediction_text='Employee Salary should be $ {}'.format(output))
+    return render_template('index.html', prediction_text='The player would be $ {}'.format(prediction))
 
 if __name__ == "__main__":
     app.run(debug=True)
